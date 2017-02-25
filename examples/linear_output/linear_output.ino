b79ff522411cc1e9f6bc064a94c9c97d9ba0a459
VasilKalchev <vase7u@gmail.com>
2017-02-25 04:06:06 +0200

    Created the examples.

---
+++ examples/linear_output/linear_output.ino
@@ -0,0 +1,72 @@
+/*
+ * ExponentMap library - linear_output.ino
+ *
+ * This example demonstrates how to actually use the object
+ * to get values for fading an LED.
+ *
+ * In this example an LED is faded in-out first using linearly
+ * growing values for the PWM, then using the exponentially
+ * growing values from the generated array. To get the values
+ * from the array use the function operator on the object or
+ * the method "stepToValue(step)".
+ *
+ * The circuit:
+ * An LED with the appropriate resistor to Arduino pin 3.
+ *
+ * Created February 25, 2017
+ * by Vasil Kalchev
+ *
+ * https://github.com/VasilKalchev/ExponentMap
+ *
+ */
+
+#include "ExponentMap.h"
+
+const byte led_pin = 3;
+
+// This is the maximum value of the PWM output. An 8 bits PWM
+// has 2^8 = 256 states, a 12 bits one has 2^12 = 4096 states.
+const byte pwm_max_value = 255;
+
+// Since we won't provide the number of steps, this variable
+// will be defined after the object calculates them.
+byte steps_count;
+
+ExponentMap<byte> e(pwm_max_value);
+
+void setup() {
+  Serial.begin(250000);
+  Serial.print("ExponentMap v."); Serial.print(EXPONENTMAP_VERSION);
+  Serial.println(" linear_output example.");
+
+  steps_count = e.stepsCount();
+}
+
+void loop() {
+  // The first two loops fade in-out the LED assigning
+  // linearly growing values (1, 2, 3, 4, 5...) to the PWM.
+  for (int i = 0; i <= pwm_max_value; i++) {
+    analogWrite(led_pin, i);
+    delay(5);
+  }
+  for (int i = pwm_max_value; i >= 0; i--) {
+    analogWrite(led_pin, i);
+    delay(5);
+  }
+
+  // The second two loops fade in-out the LED assigning
+  // exponentially growing values to the PWM.
+  for (int i = 0; i <= steps_count; i++) {
+    // Here we use the function operator on the object to get
+    // the step's corresponding value. Using e.stepToValue(i)
+    // is equivalent.
+    analogWrite(led_pin, e(i));
+    // Different delays are used because the number of steps is lower.
+    delay(75);
+  }
+  for (int i = steps_count; i >= 0; i--) {
+    analogWrite(led_pin, e(i));
+    delay(75);
+  }
+}
+
---
+++ examples/object_construction/object_construction.ino
@@ -0,0 +1,77 @@
+/*
+ * ExponentMap library - object_construction.ino
+ *
+ * This example demonstrates how to construct an object
+ * and print the generated array.
+ *
+ * Created February 25, 2017
+ * by Vasil Kalchev
+ *
+ * https://github.com/VasilKalchev/ExponentMap
+ *
+ */
+
+#include "ExponentMap.h"
+
+/*
+ * The class's constructor takes the number of steps required
+ * and the maximum value. If the number of steps is not
+ * specified, the optimal number of steps is calculated
+ * automatically. The template parameter specifies the data
+ * type for the array holding the function. If it is not
+ * specified, it will default to 32 bits unsigned integer.
+ */
+// This object will have 10 steps with a maximum value of 255.
+// The array will be of data type "byte".
+ExponentMap<byte> exp1(20, 255);
+
+// This object will calculate the optimum number of steps
+// automatically. In case of 255 maximum value, it will be 12.
+ExponentMap<byte> exp2(255);
+
+// This object will have bigger number of optimal steps.
+ExponentMap<> exp3(4096);
+
+void setup() {
+  Serial.begin(250000);
+  Serial.print("ExponentMap v."); Serial.print(EXPONENTMAP_VERSION);
+  Serial.println(" object_construction example.");
+
+  Serial.println("\nObject 'exp1':");
+  /*
+   * The method "stepsCount()" returns the number of steps. It is useful
+   * when the number of steps is not specified in the constructor.
+   */
+  Serial.print("Specified number of steps: "); Serial.println(exp1.stepsCount());
+  /*
+   * The method "stepsRepeat()" returns "true" if the specified number of steps
+   * produces a function with repeating values. To avoid that, decrease the number
+   * of steps or increase the maximum value.
+   */
+  exp1.stepsRepeat() ? Serial.println("There are repeating values. Decrease the number of steps.") : 1;
+  /*
+   * The method "printTable()" prints a table with all the steps and their
+   * corresponding value.
+   */
+  exp1.printTable();
+  /*
+   * The method "printCode()" prints the code for creating the generated array
+   * directly. This is useful if you want to get rid of the class.
+   */
+  exp1.printCode();
+
+  Serial.println("\nObject 'exp2':");
+  Serial.print("Calculated number of steps: "); Serial.println(exp2.stepsCount());
+  exp2.stepsRepeat() ? Serial.println("There are repeating values. Decrease the number of steps.") : 1;
+  exp2.printTable();
+  exp2.printCode();
+
+  Serial.println("\nObject 'exp3':");
+  Serial.print("Calculated number of steps: "); Serial.println(exp3.stepsCount());
+  exp3.stepsRepeat() ? Serial.println("There are repeating values. Decrease the number of steps.") : 1;
+  exp3.printTable();
+  exp3.printCode();
+}
+
+void loop() {}
+
